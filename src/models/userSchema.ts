65717bd614ba55086441038dce26ba52dab3c456
Shaurav Bhatt <bhattshaurav404@gmail.com>
2026-07-03 09:11:07 +0530

    feat: add user schema

---
+++ src/models/userSchema.ts
@@ -0,0 +1,54 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordTokenExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationTokenExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.models.users || mongoose.model("User", userSchema);
+
+export default User;
